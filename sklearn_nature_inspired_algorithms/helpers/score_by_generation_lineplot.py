3f6b2cd492ad48b09ef7672991aa45c0ebe409dc
Timotej Zaťko <timi.zatko@gmail.com>
2020-05-16 16:07:57 +0200

    Add possibility to select metric in lineplot

--- sklearn_nature_inspired_algorithms/helpers/score_by_generation_lineplot.py
+++ sklearn_nature_inspired_algorithms/helpers/score_by_generation_lineplot.py
@@ -6,16 +6,23 @@ import matplotlib.pyplot as plt
 
 
 def score_by_generation_lineplot(nia_search, **kwargs):
+    metrics = kwargs.get('metrics', ['min', 'max', 'mean', 'median'])
+
     df = pd.DataFrame(nia_search.optimization_logs_, columns=['generation', 'score'])
-    score_df = df.groupby('generation').agg({'score': [np.min, np.max, np.mean, np.median]}).reset_index()['score']
+    score_df = df.groupby('generation')\
+        .agg({'score': [np.min, np.max, np.mean, np.median]})\
+        .reset_index()\
+        .score\
+        .rename(columns={'amax': 'max', 'amin': 'min'})
 
     data = []
 
-    for aggregate in score_df.columns:
-        for generation, score in enumerate(score_df[aggregate]):
-            data.append((generation, aggregate, score))
+    for metric in score_df.columns:
+        for generation, score in enumerate(score_df[metric]):
+            data.append((generation, metric, score))
 
-    new_df = pd.DataFrame(data, columns=['generation', 'aggregate', 'score'])
+    new_df = pd.DataFrame(data, columns=['generation', 'metric', 'score'])
+    new_df = new_df[new_df['metric'].contains(metrics)]
 
     ax = kwargs.get('ax', None)
     ylim = kwargs.get('ylim', None)
@@ -26,4 +33,4 @@ def score_by_generation_lineplot(nia_search, **kwargs):
     if ylim is not None:
         ax.set(ylim=ylim)
 
-    return sns.lineplot(x="generation", y="score", data=new_df, hue='aggregate')
+    return sns.lineplot(x="generation", y="score", data=new_df, hue='metric')
