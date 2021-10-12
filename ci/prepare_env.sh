c20cbd93f2ea91aecbd006e82d8b16afaa407805
Timotej Zatko <timi.zatko@gmail.com>
2021-10-12 21:51:38 +0200

    Update packages
    - drop support for Python 3.6
    - add support for Python 3.10

--- ci/prepare_env.sh
+++ ci/prepare_env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python -m pip install --upgrade pip
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python
 echo "$HOME/.poetry/bin" >> "$GITHUB_PATH"
